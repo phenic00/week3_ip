c033718c473ed6f9364f5d13680a489a82f1ad95
phenic00 <otworip@gmail.com>
2022-11-06 21:01:36 +0300

    Added function for each question

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ const finishButton = document.querySelector("#submit")
 
 let total = 0;
 let score = 0;
+myAnswers.forEach(function(answer){
+    total+= parseInt(answer.value);
+});
+
+finishButton.addEventListener("click", function(e){
+ 
+    myAnswers.forEach(function(answer){
+        if (answer.checked){
+            score += parseInt(answer.value);
+        }
+   
+    })
+   
